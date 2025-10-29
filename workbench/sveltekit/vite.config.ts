39c6294db0c90451734c9f261c8e7084ee997b16
Adrian Lam <me@adriandlam.com>
2025-10-28 17:27:39 -0700

    add workbench sveltekit

---
+++ workbench/sveltekit/vite.config.ts
@@ -0,0 +1,14 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { workflowBuilderPlugin } from '@workflow/sveltekit';
+import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
+import { workflowRollupPlugin } from 'workflow/rollup-plugin';
+
+export default defineConfig({
+  plugins: [
+    sveltekit(),
+    devtoolsJson(),
+    workflowBuilderPlugin(),
+    workflowRollupPlugin(),
+  ],
+});
