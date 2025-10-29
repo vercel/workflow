dfa6f6e73d55c14373b2a244a1c3f46eb09a361b
Adrian Lam <me@adriandlam.com>
2025-10-28 22:57:30 -0700

    add test routes for sveltekit workbench

--- workbench/sveltekit/src/routes/test/+server.ts
+++
@@ -1,8 +0,0 @@
-import { start } from 'workflow/api';
-import { handleUserSignup } from '../../../workflows/user-signup.js';
-
-export const GET = async () => {
-  const run = await start(handleUserSignup, ['test@example.com']);
-
-  return new Response(String(run.runId));
-};
