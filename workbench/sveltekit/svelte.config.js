7febb8c2b259b809018746ae749ef433d6724c74
Adrian Lam <me@adriandlam.com>
2025-10-29 23:47:21 -0700

    test

--- workbench/sveltekit/svelte.config.js
+++ workbench/sveltekit/svelte.config.js
@@ -9,7 +9,7 @@ const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  kit: { adapter: node(), csrf: { trustedOrigins: ['*'] } },
+  kit: { adapter: adapter, csrf: { trustedOrigins: ['*'] } },
 };
 
 export default config;
