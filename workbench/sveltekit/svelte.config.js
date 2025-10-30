ec35febc4d7e50a6b14d18e06051b12d6df5fecf
JJ Kasper <jj@jjsweb.site>
2025-10-30 15:54:35 -0700

    update config

--- workbench/sveltekit/svelte.config.js
+++ workbench/sveltekit/svelte.config.js
@@ -2,14 +2,17 @@ import node from '@sveltejs/adapter-node';
 import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const adapter = process.env.VERCEL === '1' ? vercel() : node();
+const adapter = process.env.VERCEL_DEPLOYMENT_ID === '1' ? vercel() : node();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  kit: { adapter: adapter, csrf: { trustedOrigins: ['*'] } },
+  kit: {
+    adapter: adapter,
+    csrf: { trustedOrigins: ['*'] },
+  },
 };
 
 export default config;
