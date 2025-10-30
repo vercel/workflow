c8915d94640dfdbcd0cfb310f498bfa0dd5278dd
JJ Kasper <jj@jjsweb.site>
2025-10-30 16:30:50 -0700

    ensure vercel functions are patched

--- packages/sveltekit/src/index.ts
+++ packages/sveltekit/src/index.ts
@@ -1,15 +1,64 @@
+import fs from 'node:fs/promises';
 import { LocalBuilder } from './builders.js';
 
-await new LocalBuilder({}).build();
+const localBuilder = new LocalBuilder({});
+
+// This needs to be in the top-level as we need to create these
+// entries before svelte plugin is started or the entries are
+// a race to be created before svelte discovers entries
+await localBuilder.build();
 
 export function workflowPlugin() {
   return {
     name: 'workflow-sveltekit-plugin',
-    // async buildEnd() {
-    //   if (process.env.VERCEL === '1') {
-    //     await new VercelBuilder({}).build();
-    //   }
-    // },
+    async writeBundle() {
+      // don't attempt patching functions output if not Vercel adapter
+      if (!process.env.VERCEL_DEPLOYMENT_ID) {
+        return;
+      }
+
+      for (const { file, config } of [
+        {
+          file: '.vercel/output/functions/.well-known/workflow/v1/flow.func/.vc-config.json',
+          config: {
+            experimentalTriggers: [
+              {
+                type: 'queue/v1beta',
+                topic: '__wkf_workflow_*',
+                consumer: 'default',
+                maxDeliveries: 64,
+                retryAfterSeconds: 5,
+                initialDelaySeconds: 0,
+              },
+            ],
+          },
+        },
+        {
+          file: '.vercel/output/functions/.well-known/workflow/v1/step.func/.vc-config.json',
+          config: {
+            experimentalTriggers: [
+              {
+                type: 'queue/v1beta',
+                topic: '__wkf_step_*',
+                consumer: 'default',
+                maxDeliveries: 64,
+                retryAfterSeconds: 5,
+                initialDelaySeconds: 0,
+              },
+            ],
+          },
+        },
+      ]) {
+        const existingConfig = JSON.parse(await fs.readFile(file, 'utf8'));
+        await fs.writeFile(
+          file,
+          JSON.stringify({
+            ...existingConfig,
+            config,
+          })
+        );
+      }
+    },
   };
 }
 
