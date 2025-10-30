1438ac6235931cf7889b234e4d6e77ff329aad1a
Adrian Lam <me@adriandlam.com>
2025-10-29 19:12:32 -0700

    refactor: workflowPlugin for sveltekit

--- workbench/sveltekit/vite.config.ts
+++ workbench/sveltekit/vite.config.ts
@@ -1,12 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { workflowBuilderPlugin } from '@workflow/sveltekit';
+import { workflowPlugin } from '@workflow/sveltekit';
 import { defineConfig } from 'vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { workflowRollupPlugin } from 'workflow/rollup-plugin';
 
 export default defineConfig({
   plugins: [
-    workflowBuilderPlugin(),
+    workflowPlugin(),
     workflowRollupPlugin(),
     devtoolsJson(),
     sveltekit(),
