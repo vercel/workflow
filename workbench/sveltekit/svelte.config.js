00d487d3b6033c7a884b4d5d953a213f63d20e8c
Adrian Lam <me@adriandlam.com>
2025-10-29 23:10:31 -0700

    fix: sveltekit workbench adapter

--- workbench/sveltekit/svelte.config.js
+++ workbench/sveltekit/svelte.config.js
@@ -1,12 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import autoAdapter from '@sveltejs/adapter-auto';
+import nodeAdapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const adapter = process.env.VERCEL === '1' ? autoAdapter() : nodeAdapter();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  kit: { adapter: adapter(), csrf: { trustedOrigins: ['*'] } },
+  kit: { adapter: adapter, csrf: { trustedOrigins: ['*'] } },
 };
 
 export default config;
