49fb050e8b3ece109e3df738a86655a25d92619b
Pooya Parsa <pooya@pi0.io>
2025-10-30 22:37:56 +0100

    switch entry format to node

--- workbench/express/nitro.config.ts
+++ workbench/express/nitro.config.ts
@@ -3,6 +3,6 @@ import { defineNitroConfig } from 'nitro/config';
 export default defineNitroConfig({
   preset: 'vercel',
   modules: ['workflow/nitro'],
-  vercel: { entryFormat: "web" },
+  vercel: { entryFormat: "node" },
   handlers: [ { route: '/api/**', handler: 'src/index.ts' } ],
 });
