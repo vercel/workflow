bdbcba7cdaaa2d2e0b50146f8762bbfc0ce91ea7
Adrian Lam <me@adriandlam.com>
2025-10-30 15:38:57 -0700

    add tanstack project

---
+++ workbench/tanstack/src/routes/api/hook.ts
@@ -0,0 +1,31 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { getHookByToken, resumeHook } from 'workflow/api';
+
+export const Route = createFileRoute('/api/hook')({
+  server: {
+    handlers: {
+      GET: async ({ request }) => {
+        const { token, data } = await request.json();
+
+        let hook: Awaited<ReturnType<typeof getHookByToken>>;
+        try {
+          hook = await getHookByToken(token);
+          console.log('hook', hook);
+        } catch (error) {
+          console.log('error during getHookByToken', error);
+          // TODO: `WorkflowAPIError` is not exported, so for now
+          // we'll return 404 assuming it's the "invalid" token test case
+          return Response.json(null, { status: 404 });
+        }
+
+        await resumeHook(hook.token, {
+          ...data,
+          // @ts-expect-error metadata is not typed
+          customData: hook.metadata?.customData,
+        });
+
+        return Response.json(hook);
+      },
+    },
+  },
+});
---
+++ workbench/tanstack/vite.config.ts
@@ -0,0 +1,25 @@
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import { nitro } from 'nitro/vite';
+import { defineConfig } from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+const config = defineConfig({
+  plugins: [
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    nitro({
+      config: {
+        modules: ['workflow/nitro'],
+      },
+    }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+});
+
+export default config;
