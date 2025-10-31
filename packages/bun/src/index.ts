8bfba5762033b805bf6a77744f72de55bb60f2ce
Adrian Lam <me@adriandlam.com>
2025-10-31 12:35:36 -0700

    fix: build workflow routes on bun import

--- packages/bun/src/index.ts
+++ packages/bun/src/index.ts
@@ -1,17 +1,13 @@
-import { writeFileSync } from 'node:fs';
-import { mkdir } from 'node:fs/promises';
-import { join } from 'node:path';
 import { transform } from '@swc/core';
-import { BaseBuilder } from '@workflow/cli/dist/lib/builders/base-builder';
-import type { WorkflowConfig } from '@workflow/cli/dist/lib/config/types';
 import type { BunPlugin } from 'bun';
+import { LocalBuilder } from './builders';
+
+await new LocalBuilder().build();
 
 export function workflowPlugin(): BunPlugin {
   return {
     name: 'workflow-plugin',
     async setup(build) {
-      await new LocalBuilder().build();
-
       // Client transform plugin - only transform TypeScript files
       // JS files are already built
       build.onLoad({ filter: /\.(ts|tsx)$/ }, async (args) => {
@@ -35,49 +31,4 @@ export function workflowPlugin(): BunPlugin {
       });
     },
   };
-}
-
-export class LocalBuilder extends BaseBuilder {
-  #outDir: string;
-  constructor(config?: Partial<WorkflowConfig>) {
-    const outDir = join(config?.workingDir ?? process.cwd(), '.workflows');
-    super({
-      buildTarget: 'standalone' as const,
-      stepsBundlePath: '',
-      workflowsBundlePath: '',
-      webhookBundlePath: '',
-      dirs: config?.dirs ?? ['./workflows'],
-      workingDir: config?.workingDir ?? process.cwd(),
-    });
-    this.#outDir = outDir;
-  }
-
-  override async build(): Promise<void> {
-    const inputFiles = await this.getInputFiles();
-    await mkdir(this.#outDir, { recursive: true });
-
-    await this.createWorkflowsBundle({
-      outfile: join(this.#outDir, 'workflows.js'),
-      bundleFinalOutput: false,
-      format: 'esm',
-      inputFiles,
-    });
-
-    await this.createStepsBundle({
-      outfile: join(this.#outDir, 'steps.js'),
-      externalizeNonSteps: true,
-      format: 'esm',
-      inputFiles,
-    });
-
-    await this.createWebhookBundle({
-      outfile: join(this.#outDir, 'webhook.js'),
-      bundle: false,
-    });
-
-    console.log('Created webhook bundle');
-
-    // Add .workflows to .gitignore
-    writeFileSync(join(this.#outDir, '.gitignore'), '*\n');
-  }
 }
