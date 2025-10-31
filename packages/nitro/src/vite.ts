19905430854ae57992fb07ccc0e8214047615cfe
Pooya Parsa <pooya@pi0.io>
2025-10-31 21:38:59 +0100

    allow passing options via plugin

--- packages/nitro/src/vite.ts
+++ packages/nitro/src/vite.ts
@@ -1,14 +1,22 @@
 import type { Plugin } from 'vite'
+import type { ModuleOptions } from './index.js'
+import { Nitro } from 'nitro/types';
 import { workflowRollupPlugin } from './rollup-plugin.js';
 import nitroModule from './index.js'
 
-export function workflow(): Plugin[] {
+export function workflow(options?: ModuleOptions): Plugin[] {
   return [
     workflowRollupPlugin(),
     {
       name: 'workflow:nitro',
+      // Requires https://github.com/nitrojs/nitro/discussions/3680
       // @ts-ignore
-      nitro: nitroModule
+      nitro: {
+        setup: (nitro: Nitro) => {
+          nitro.options.workflow = { ...nitro.options.workflow, ...options }
+          return nitroModule.setup(nitro)
+        }
+      }
     }
   ]
 }
