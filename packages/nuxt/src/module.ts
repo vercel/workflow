611c89ea2e94f76651148742e50f4e74126b30ab
Daniel Roe <daniel@roe.dev>
2025-11-03 16:46:24 +0000

    feat: enable typescript plugin in tsconfig

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -1,7 +1,13 @@
 import { defineNuxtModule } from '@nuxt/kit';
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  /**
+   * Enable TypeScript plugin for workflow
+   * @default true
+   */
+  typescriptPlugin: boolean;
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
@@ -10,12 +16,27 @@ export default defineNuxtModule<ModuleOptions>({
     docs: 'https://useworkflow.dev/docs/getting-started/nuxt',
   },
   // Default configuration options of the Nuxt module
-  defaults: {},
-  setup(_options, nuxt) {
+  defaults: {
+    typescriptPlugin: true,
+  },
+  setup(options, nuxt) {
     nuxt.options.nitro ||= {};
     nuxt.options.nitro.modules ||= [];
+
     if (!nuxt.options.nitro.modules.includes('@workflow/nitro')) {
       nuxt.options.nitro.modules.push('@workflow/nitro');
+    }
+
+    if (options.typescriptPlugin) {
+      nuxt.hook('nitro:config', (config) => {
+        config.typescript ||= {};
+        config.typescript.tsConfig ||= {};
+        config.typescript.tsConfig.compilerOptions ||= {};
+        config.typescript.tsConfig.compilerOptions.plugins ||= [];
+        config.typescript.tsConfig.compilerOptions.plugins.push({
+          name: 'workflow',
+        });
+      });
     }
   },
 });
