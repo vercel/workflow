e7ae011f6bf019008794345e2eb6dd905da53342
Adrian Lam <me@adriandlam.com>
2025-11-03 23:27:39 -0800

    fix: test confnigs

--- packages/core/e2e/sveltekit-dev.test.ts
+++ packages/core/e2e/sveltekit-dev.test.ts
@@ -4,5 +4,5 @@ createDevTests({
   generatedStepPath: 'src/routes/.well-known/workflow/v1/step/+server.js',
   generatedWorkflowPath: 'src/routes/.well-known/workflow/v1/flow/+server.js',
   apiFilePath: 'src/routes/api/chat/+server.ts',
-  apiFileImportPath: '../../../../',
+  apiFileImportPath: '../../../..',
 });
