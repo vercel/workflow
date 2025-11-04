7737691a2a43ebbd2fbeef686c4e80b5d3a8dc72
Adrian Lam <me@adriandlam.com>
2025-11-04 09:56:53 -0800

    refactor(rollup): change rollup export path

--- packages/nitro/src/vite.ts
+++ packages/nitro/src/vite.ts
@@ -1,8 +1,8 @@
-import type { Plugin } from 'vite'
-import type { ModuleOptions } from './index.js'
 import type { Nitro } from 'nitro/types';
-import { workflowRollupPlugin } from './rollup-plugin.js';
-import nitroModule from './index.js'
+import type { Plugin } from 'vite';
+import type { ModuleOptions } from './index.js';
+import nitroModule from './index.js';
+import { workflowRollupPlugin } from './rollup.js';
 
 export function workflow(options?: ModuleOptions): Plugin[] {
   return [
@@ -10,13 +10,17 @@ export function workflow(options?: ModuleOptions): Plugin[] {
     {
       name: 'workflow:nitro',
       // Requires https://github.com/nitrojs/nitro/discussions/3680
-      // @ts-ignore
+      // @ts-expect-error
       nitro: {
         setup: (nitro: Nitro) => {
-          nitro.options.workflow = { ...nitro.options.workflow, ...options, _vite: true }
-          return nitroModule.setup(nitro)
-        }
-      }
-    }
-  ]
+          nitro.options.workflow = {
+            ...nitro.options.workflow,
+            ...options,
+            _vite: true,
+          };
+          return nitroModule.setup(nitro);
+        },
+      },
+    },
+  ];
 }
