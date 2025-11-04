1ce2ac5e41870beaeab984b02345450c22dfedd7
Adrian Lam <me@adriandlam.com>
2025-11-03 22:15:02 -0800

    add hmr and dev tests for nitro and sveltekit

---
+++ packages/core/e2e/nitro-dev.test.ts
@@ -0,0 +1,8 @@
+import { createDevTests } from './dev-test-factory';
+
+createDevTests({
+  generatedStepPath: '.nitro/workflow/steps.mjs',
+  generatedWorkflowPath: '.nitro/workflow/workflows.mjs',
+  apiFilePath: 'routes/api/chat.post.ts',
+  apiFileImportPath: '../../',
+});
---
+++ workbench/sveltekit/src/routes/api/chat/+server.ts
@@ -0,0 +1,14 @@
+// THIS FILE IS JUST FOR TESTING HMR AS AN ENTRY NEEDS
+// TO IMPORT THE WORKFLOWS TO DISCOVER THEM AND WATCH
+
+import { json, type RequestHandler } from '@sveltejs/kit';
+import * as workflows from '../../../../workflows/streams';
+
+export const POST: RequestHandler = async ({
+  request,
+}: {
+  request: Request;
+}) => {
+  console.log(workflows);
+  return json('hello world');
+};
