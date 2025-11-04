252de2141da20d8877627265b67ab6d6ca334da9
Adrian Lam <me@adriandlam.com>
2025-11-03 22:23:09 -0800

    revert: e2e testing code

--- workbench/example/workflows/99_e2e.ts
+++ workbench/example/workflows/99_e2e.ts
@@ -395,13 +395,3 @@ export async function retryableAndFatalErrorWorkflow() {
 
   return { retryableResult, gotFatalError };
 }
-
-export async function myNewWorkflow() {
-  'use workflow';
-  return 'hello world';
-}
-
-export async function myNewStep() {
-  'use step';
-  return 'hello world';
-}
