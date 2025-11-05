184d7221e2c015a5d208e407f4d796be320ca04c
Adrian Lam <me@adriandlam.com>
2025-11-04 17:58:50 -0800

    chore: add comment for info

--- packages/sveltekit/src/builder.ts
+++ packages/sveltekit/src/builder.ts
@@ -160,6 +160,7 @@ export const POST = async ({request}) => {
     // Post-process the generated file to wrap with SvelteKit request converter
     let webhookRouteContent = await readFile(webhookRouteFile, 'utf-8');
 
+    // NOTE: This is a workaround to avoid crashing in local dev when context isn't set for waitUntil()
     webhookRouteContent = `process.on('unhandledRejection', (reason) => { if (reason !== undefined) console.error('Unhandled rejection detected', reason); });
 ${webhookRouteContent}`;
 
