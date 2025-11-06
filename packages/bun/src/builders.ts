1b97251c99232f2129d7d17fceefbccac5676991
Adrian Lam <me@adriandlam.com>
2025-11-05 21:57:39 -0800

    fix: add hack to ignore undefined unhandled rejections

--- packages/bun/src/builders.ts
+++ packages/bun/src/builders.ts
@@ -1,5 +1,5 @@
 import { writeFileSync } from 'node:fs';
-import { mkdir } from 'node:fs/promises';
+import { mkdir, readFile } from 'node:fs/promises';
 import { join } from 'node:path';
 import { BaseBuilder } from '@workflow/cli/dist/lib/builders/base-builder';
 import type { WorkflowConfig } from '@workflow/cli/dist/lib/config/types';
@@ -41,6 +41,17 @@ export class LocalBuilder extends BaseBuilder {
       outfile: join(this.#outDir, 'webhook.js'),
       bundle: false,
     });
+
+    let webhookRouteContent = await readFile(
+      join(this.#outDir, 'webhook.js'),
+      'utf-8'
+    );
+
+    // NOTE: This is a workaround to avoid crashing in local dev when context isn't set for waitUntil()
+    webhookRouteContent = `process.on('unhandledRejection', (reason) => { if (reason !== undefined) console.error('Unhandled rejection detected', reason); });
+${webhookRouteContent}`;
+
+    writeFileSync(join(this.#outDir, 'webhook.js'), webhookRouteContent);
 
     console.log('Created webhook bundle');
 
