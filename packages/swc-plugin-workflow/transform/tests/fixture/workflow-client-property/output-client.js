e0c6618bd68d8caf3919fefa4a8b2521fdf60b3c
Adrian <me@adriandlam.com>
2025-11-06 14:11:31 -0800

    fix(swc-plugin): specify import path of start function on error (#244)

--- packages/swc-plugin-workflow/transform/tests/fixture/workflow-client-property/output-client.js
+++ packages/swc-plugin-workflow/transform/tests/fixture/workflow-client-property/output-client.js
@@ -1,20 +1,20 @@
 // Test workflow functions in client mode
 /**__internal_workflows{"workflows":{"input.js":{"arrowWorkflow":{"workflowId":"workflow//input.js//arrowWorkflow"},"defaultWorkflow":{"workflowId":"workflow//input.js//defaultWorkflow"},"internalWorkflow":{"workflowId":"workflow//input.js//internalWorkflow"},"myWorkflow":{"workflowId":"workflow//input.js//myWorkflow"}}}}*/;
 export async function myWorkflow() {
-    throw new Error("You attempted to execute workflow myWorkflow function directly. To start a workflow, use start(myWorkflow) from workflow");
+    throw new Error("You attempted to execute workflow myWorkflow function directly. To start a workflow, use start(myWorkflow) from workflow/api");
 }
 myWorkflow.workflowId = "workflow//input.js//myWorkflow";
 export const arrowWorkflow = async ()=>{
-    throw new Error("You attempted to execute workflow arrowWorkflow function directly. To start a workflow, use start(arrowWorkflow) from workflow");
+    throw new Error("You attempted to execute workflow arrowWorkflow function directly. To start a workflow, use start(arrowWorkflow) from workflow/api");
 };
 arrowWorkflow.workflowId = "workflow//input.js//arrowWorkflow";
 export default async function defaultWorkflow() {
-    throw new Error("You attempted to execute workflow defaultWorkflow function directly. To start a workflow, use start(defaultWorkflow) from workflow");
+    throw new Error("You attempted to execute workflow defaultWorkflow function directly. To start a workflow, use start(defaultWorkflow) from workflow/api");
 }
 defaultWorkflow.workflowId = "workflow//input.js//defaultWorkflow";
 // Non-export workflow function
 async function internalWorkflow() {
-    throw new Error("You attempted to execute workflow internalWorkflow function directly. To start a workflow, use start(internalWorkflow) from workflow");
+    throw new Error("You attempted to execute workflow internalWorkflow function directly. To start a workflow, use start(internalWorkflow) from workflow/api");
 }
 internalWorkflow.workflowId = "workflow//input.js//internalWorkflow";
 // Use the internal workflow to avoid lint warning
