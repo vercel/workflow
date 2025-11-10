dd2469267467cc5b387dfe8ee0e2aaff0645169e
Pooya Parsa <pooya@pi0.io>
2025-11-10 22:49:06 +0100

    refactor: migrate to latest nitro v3
    Signed-off-by:  Pooya Parsa <pooya@pi0.io>

--- workbench/nitro-v3/nitro.config.ts
+++ workbench/nitro-v3/nitro.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from 'nitro';
 
+<<<<<<< HEAD
 export default defineConfig({
   modules: ['workflow/nitro'],
   serverDir: './',
+=======
+export default defineNitroConfig({
+  serverDir: './',
+  modules: [
+    'workflow/nitro'
+  ],
+  // Workaround for monorepo symlinked packages
+  externals: {
+    external: [id => id.includes('.nitro/workflow')]
+  },
+>>>>>>> 1c3c8731 (refactor: migrate to latest nitro v3)
 });
