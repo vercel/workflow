28cedf7dcac9e630a563dac17a2432da687713a8
Pooya Parsa <pooya@pi0.io>
2025-11-10 23:09:06 +0100

    update workaround

--- workbench/nitro-v3/nitro.config.ts
+++ workbench/nitro-v3/nitro.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from "nitro";
 
+<<<<<<< HEAD
 export default defineConfig({
   modules: ["workflow/nitro"],
   serverDir: "./",
+=======
+export default defineNitroConfig({
+  serverDir: './',
+  modules: [
+    'workflow/nitro'
+  ],
+  // Workaround for monorepo symlinks
+  noExternals: true,
+  rollupConfig: {
+    watch: {
+      exclude: ['**/.workflow-data/**', '**/node_modules/**']
+    }
+  }
+>>>>>>> 2d31493b (update workaround)
 });
