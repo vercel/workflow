4fc16a983981f4eb875a3b2932b66db81709bb01
Pooya Parsa <pooya@pi0.io>
2025-11-10 23:09:06 +0100

    update workaround

--- workbench/nitro-v3/nitro.config.ts
+++ workbench/nitro-v3/nitro.config.ts
@@ -1,18 +1,6 @@
-import { defineConfig } from 'nitro';
+import { defineConfig } from "nitro";
 
-<<<<<<< HEAD
 export default defineConfig({
-  modules: ['workflow/nitro'],
-  serverDir: './',
-=======
-export default defineNitroConfig({
-  serverDir: './',
-  modules: [
-    'workflow/nitro'
-  ],
-  // Workaround for monorepo symlinked packages
-  externals: {
-    external: [id => id.includes('.nitro/workflow')]
-  },
->>>>>>> 1c3c8731 (refactor: migrate to latest nitro v3)
+  modules: ["workflow/nitro"],
+  serverDir: "./",
 });
