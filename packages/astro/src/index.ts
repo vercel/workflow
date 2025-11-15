ea3681be41cb3f794364a7f6f4257ba615e46eaa
Adrian Lam <me@adriandlam.com>
2025-11-14 18:48:44 -0800

    chore: cleanup

--- packages/astro/src/index.ts
+++ packages/astro/src/index.ts
@@ -1,10 +1,1 @@
-// import { LocalBuilder } from './builder.js';
-
-// const builder = new LocalBuilder();
-
-// // This needs to be in the top-level as we need to create these
-// // entries before astro plugin is started or the entries are
-// // a race to be created before astro discovers entries
-// await builder.build();
-
 export { workflowPlugin } from './plugin.js';
