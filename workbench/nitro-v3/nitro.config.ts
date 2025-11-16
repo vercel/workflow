e3ca57e53e502978e0db2a25fd4e390f063b422c
Adrian Lam <me@adriandlam.com>
2025-11-15 18:54:46 -0800

    fix: add back nitro config

--- workbench/nitro-v3/nitro.config.ts
+++ workbench/nitro-v3/nitro.config.ts
@@ -2,14 +2,14 @@ import { defineNitroConfig } from 'nitro/config';
 
 export default defineNitroConfig({
   serverDir: './',
-  modules: [
-    'workflow/nitro'
-  ],
-  // Workaround for monorepo symlinks
-  noExternals: true,
+  modules: ['workflow/nitro'],
+  // Workaround for monorepo symlinked packages
+  externals: {
+    external: [(id) => id.includes('.nitro/workflow')],
+  },
   rollupConfig: {
     watch: {
-      exclude: ['**/.workflow-data/**', '**/node_modules/**']
-    }
-  }
+      exclude: ['**/.workflow-data/**', '**/node_modules/**'],
+    },
+  },
 });
