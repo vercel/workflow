8ce8737ca1bdacdb3fe3b655774496e55253d523
Pooya Parsa <pooya@pi0.io>
2025-11-17 21:56:39 +0100

    update docs and workbench

--- workbench/hono/nitro.config.ts
+++ workbench/hono/nitro.config.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { defineConfig } from "nitro";
 
 <<<<<<< HEAD
@@ -16,4 +17,13 @@ export default defineNitroConfig({
     },
   ],
 >>>>>>> 4bdc6c3a (fix(nitro): externalize .nitro/workflow folder)
+=======
+import { defineConfig } from 'nitro';
+
+export default defineConfig({
+  modules: ['workflow/nitro'],
+  routes: {
+    '/**': './src/index.ts',
+  },
+>>>>>>> be993557 (update docs and workbench)
 });
--- workbench/nitro-v3/nitro.config.ts
+++ workbench/nitro-v3/nitro.config.ts
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 import { defineConfig } from "nitro";
 
 export default defineConfig({
   modules: ["workflow/nitro"],
   serverDir: "./",
+=======
+import { defineConfig } from 'nitro';
+
+export default defineConfig({
+  modules: ['workflow/nitro'],
+  serverDir: './',
+>>>>>>> be993557 (update docs and workbench)
 });
