b537f34b4c5b4d0952c9692fe9fe1744dc86e257
Adrian Lam <me@adriandlam.com>
2025-11-17 18:36:39 -0800

    format

--- workbench/nitro-v3/nitro.config.ts
+++ workbench/nitro-v3/nitro.config.ts
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-import { defineConfig } from "nitro";
-
-export default defineConfig({
-  modules: ["workflow/nitro"],
-  serverDir: "./",
-=======
 import { defineConfig } from 'nitro';
 
 export default defineConfig({
   modules: ['workflow/nitro'],
   serverDir: './',
->>>>>>> be993557 (update docs and workbench)
 });
--- workbench/vite/vite.config.ts
+++ workbench/vite/vite.config.ts
@@ -1,14 +1,10 @@
-import { nitro } from "nitro/vite";
-import { defineConfig } from "vite";
-import { workflow } from "workflow/vite";
+import { nitro } from 'nitro/vite';
+import { defineConfig } from 'vite';
+import { workflow } from 'workflow/vite';
 
 export default defineConfig({
   plugins: [nitro(), workflow()],
   nitro: {
-<<<<<<< HEAD
-    serverDir: "./",
-=======
     serverDir: './',
->>>>>>> d74fde7b (chore: format)
   },
 });
