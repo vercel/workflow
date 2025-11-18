1502135eb4b7a9fca2ddaa4ef8d80b61b7044fa5
Nathan Rajlich <n@n8.io>
2025-11-18 00:37:32 -0800

    WIP factory-with-step-method fixture

---
+++ packages/swc-plugin-workflow/transform/tests/fixture/factory-with-step-method/output-step.js
@@ -0,0 +1,8 @@
+import fs from 'fs/promises';
+const myFactory = ()=>({
+        myStep: async ()=>{
+            'use step';
+            await fs.mkdir('test');
+        }
+    });
+export default myFactory;
