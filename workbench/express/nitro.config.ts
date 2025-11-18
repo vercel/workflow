a3802c0adfc6959b1d42715fbe478cc8f27582c9
Adrian Lam <me@adriandlam.com>
2025-11-17 16:05:15 -0800

    fix: express nitro config

--- workbench/express/nitro.config.ts
+++ workbench/express/nitro.config.ts
@@ -1,8 +1,9 @@
 import { defineNitroConfig } from 'nitro/config';
 
 export default defineNitroConfig({
-  srcDir: 'src',
   modules: ['workflow/nitro'],
   vercel: { entryFormat: 'node' },
-  handlers: [{ route: '/api/**', handler: 'src/index.ts' }],
+  routes: {
+    '/**': './src/index.ts',
+  },
 });
