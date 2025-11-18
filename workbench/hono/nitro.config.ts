ab106c9b73fbc8fac36bf1b6e24b34e8eb9cff36
Adrian Lam <me@adriandlam.com>
2025-11-17 18:38:24 -0800

    fix

--- workbench/hono/nitro.config.ts
+++ workbench/hono/nitro.config.ts
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import { defineConfig } from "nitro";
-
-<<<<<<< HEAD
-export default defineConfig({
-  modules: ["workflow/nitro"],
-  routes: {
-    "/**": "./src/index.ts",
-  },
-=======
-export default defineNitroConfig({
-  modules: ['workflow/nitro'],
-  handlers: [
-    {
-      route: '/api/**',
-      handler: './server.ts',
-    },
-  ],
->>>>>>> 4bdc6c3a (fix(nitro): externalize .nitro/workflow folder)
-=======
 import { defineConfig } from 'nitro';
 
 export default defineConfig({
@@ -25,5 +5,4 @@ export default defineConfig({
   routes: {
     '/**': './src/index.ts',
   },
->>>>>>> be993557 (update docs and workbench)
 });
