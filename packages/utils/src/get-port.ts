cde4a51b9a2372f8bf69ff132665d11891c41f9a
Adrian Lam <me@adriandlam.com>
2025-11-20 11:12:34 -0800

    remove pid-port fallback

--- packages/utils/src/get-port.ts
+++ packages/utils/src/get-port.ts
@@ -1,6 +1,5 @@
 import { exec } from 'node:child_process';
 import { promisify } from 'node:util';
-import { pidToPorts } from 'pid-port';
 
 const execAsync = promisify(exec);
 
@@ -10,33 +9,28 @@ const execAsync = promisify(exec);
  * NOTE: Can't move this to @workflow/utils because it's being imported into @workflow/errors for RetryableError (inside workflow runtime)
  */
 export async function getPort(): Promise<number | undefined> {
+  const pid = process.pid;
+  const platform = process.platform;
+
+  let port: number | undefined;
+
   try {
-    const ports = Array.from(await pidToPorts(process.pid));
-
-    // pid-port failed to detect the server port
-    if (ports.length === 0 || ports.length > 1) {
-      const platform = process.platform;
-
-      // Use our fallback
-      switch (platform) {
-        case 'linux':
-        case 'darwin': {
-          const result = await execAsync(
-            `lsof -i -P -n | grep -w ${process.pid} | grep LISTEN | awk '{print $9}' | sed 's/.*://' | head -n 1`
-          );
-          const port = parseInt(result.stdout.trim(), 10);
-          return Number.isNaN(port) ? undefined : port;
-        }
-        case 'win32':
-          throw new Error('Not implemented');
+    // Use our fallback
+    switch (platform) {
+      case 'linux':
+      case 'darwin': {
+        const result = await execAsync(
+          `lsof -i -P -n | grep -w ${pid} | grep LISTEN | awk '{print $9}' | sed 's/.*://' | head -n 1`
+        );
+        port = parseInt(result.stdout.trim(), 10);
+        break;
       }
-
-      return undefined;
+      case 'win32':
+        throw new Error('Not implemented');
     }
-
-    // Server port most likely to be minimum
-    return Math.min(...ports);
   } catch {
     return undefined;
   }
+
+  return Number.isNaN(port) ? undefined : port;
 }
