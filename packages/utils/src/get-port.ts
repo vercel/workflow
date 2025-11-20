c72742b08c260b895ca6289260479b47da9781b8
Adrian Lam <me@adriandlam.com>
2025-11-19 22:13:06 -0800

    test new pid to port cmd

--- packages/utils/src/get-port.ts
+++ packages/utils/src/get-port.ts
@@ -1,23 +1,35 @@
-import { pidToPorts } from 'pid-port';
+// import { pidToPorts } from "pid-port";
+import { exec } from 'node:child_process';
+import { promisify } from 'node:util';
+
+const execAsync = promisify(exec);
 
 /**
  * Gets the port number that the process is listening on.
  * @returns The port number that the process is listening on, or undefined if the process is not listening on any port.
  * NOTE: Can't move this to @workflow/utils because it's being imported into @workflow/errors for RetryableError (inside workflow runtime)
  */
-export async function getPort(): Promise<number | undefined> {
-  try {
-    const pid = process.pid;
-    const ports = await pidToPorts(pid);
-    if (!ports || ports.size === 0) {
-      return undefined;
-    }
+export async function getPort(): Promise<number> {
+  const pid = process.pid;
+  const platform = process.platform;
 
-    const smallest = Math.min(...ports);
-    return smallest;
-  } catch {
-    // If port detection fails (e.g., `ss` command not available in production),
-    // return undefined and fall back to default port
-    return undefined;
+  let port: number | undefined;
+  switch (platform) {
+    case 'linux':
+    case 'darwin': {
+      const result = await execAsync(
+        `lsof -i -P -n | grep -w ${pid} | grep LISTEN | awk '{print $9}' | sed 's/.*://' | head -n 1`
+      );
+      port = parseInt(result.stdout.trim(), 10);
+      break;
+    }
+    case 'win32':
+      throw new Error('Not implemented');
   }
+
+  if (!port) {
+    throw new Error('Failed to detect port');
+  }
+
+  return port;
 }
