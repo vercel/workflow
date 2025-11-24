f302405ef8fe4ac67bc45e5f4003485c6811423e
Adrian Lam <me@adriandlam.com>
2025-11-24 10:46:40 -0800

    revert

--- workbench/sveltekit/svelte.config.js
+++ workbench/sveltekit/svelte.config.js
@@ -3,11 +3,7 @@ import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 // Node adapter needed for ci tests
-const adapter = process.env.VERCEL_DEPLOYMENT_ID
-  ? vercel({
-      runtime: 'nodejs22.x',
-    })
-  : node();
+const adapter = process.env.VERCEL_DEPLOYMENT_ID ? vercel() : node();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
