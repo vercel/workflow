3f4a23ca67dbe3a91533969c233dd7062b65e3a4
Adrian <me@adriandlam.com>
2025-11-23 19:02:04 -0800

    Update workbench/vite/vite.config.ts
    Co-authored-by: vercel[bot] <35613825+vercel[bot]@users.noreply.github.com>

--- workbench/vite/vite.config.ts
+++ workbench/vite/vite.config.ts
@@ -11,10 +11,9 @@ export default defineConfig({
       // Needed since we test this in CI
       compiled: async () => {
         if (process.env.WORKFLOW_TARGET_WORLD === '@workflow/world-postgres') {
-          import('workflow/runtime').then(async ({ getWorld }) => {
-            console.log('Starting Postgres World...');
-            await getWorld().start?.();
-          });
+          const { getWorld } = await import('workflow/runtime');
+          console.log('Starting Postgres World...');
+          await getWorld().start?.();
         }
       },
     },
