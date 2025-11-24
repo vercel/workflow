d9e60ef446e95024f4733c85ab9caeabcc396b01
Adrian Lam <me@adriandlam.com>
2025-11-23 19:08:32 -0800

    fix nuxt plugin

--- workbench/nuxt/server/plugins/start-pg-world.ts
+++ workbench/nuxt/server/plugins/start-pg-world.ts
@@ -1,6 +1,8 @@
+import { defineNuxtPlugin } from 'nuxt/app';
+
 // Start the Postgres World
 // Needed since we test this in CI
-export default defineNitroPlugin(async (nitroApp) => {
+export default defineNuxtPlugin(() => {
   if (process.env.WORKFLOW_TARGET_WORLD === '@workflow/world-postgres') {
     import('workflow/runtime').then(async ({ getWorld }) => {
       console.log('Starting Postgres World...');
