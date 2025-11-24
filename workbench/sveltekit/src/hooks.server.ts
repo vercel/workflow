25e714addd644843fceee4d52e3428ae84f4aef8
Adrian <me@adriandlam.com>
2025-11-23 19:01:54 -0800

    Update workbench/sveltekit/src/hooks.server.ts
    Co-authored-by: vercel[bot] <35613825+vercel[bot]@users.noreply.github.com>

--- workbench/sveltekit/src/hooks.server.ts
+++ workbench/sveltekit/src/hooks.server.ts
@@ -4,9 +4,8 @@ export const init: ServerInit = async () => {
   // Start the Postgres World
   // Needed since we test this in CI
   if (process.env.WORKFLOW_TARGET_WORLD === '@workflow/world-postgres') {
-    import('workflow/runtime').then(async ({ getWorld }) => {
-      console.log('Starting Postgres World...');
-      await getWorld().start?.();
-    });
+    const { getWorld } = await import('workflow/runtime');
+    console.log('Starting Postgres World...');
+    await getWorld().start?.();
   }
 };
