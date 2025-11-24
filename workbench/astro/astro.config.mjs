29a01833bca0e0fd78a3d95680bc61eb64dce41e
Adrian Lam <me@adriandlam.com>
2025-11-24 09:58:28 -0800

    feat: add postgres world init to astro config

--- workbench/astro/astro.config.mjs
+++ workbench/astro/astro.config.mjs
@@ -13,7 +13,26 @@ const adapter = process.env.VERCEL_DEPLOYMENT_ID
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [workflow()],
+  integrations: [
+    workflow(),
+    {
+      name: 'workflow-init-pg-world',
+      hooks: {
+        // Start the Postgres World
+        // Needed since we test this in CI
+        'astro:server:setup': async () => {
+          if (
+            process.env.WORKFLOW_TARGET_WORLD === '@workflow/world-postgres'
+          ) {
+            import('workflow/runtime').then(async ({ getWorld }) => {
+              console.log('Starting Postgres World...');
+              await getWorld().start?.();
+            });
+          }
+        },
+      },
+    },
+  ],
   adapter: adapter,
   // WARNING: CSRF protection is disabled for testing/development purposes.
   // This configuration trusts all origins and should NOT be used in production.
