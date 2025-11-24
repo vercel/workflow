cbd4f9c390789e2874a1ff2349fbf3ca2dfe2702
Adrian Lam <me@adriandlam.com>
2025-11-23 19:38:29 -0800

    CI WILL BE GREEN

--- workbench/vite/vite.config.ts
+++ workbench/vite/vite.config.ts
@@ -6,16 +6,6 @@ export default defineConfig({
   plugins: [nitro(), workflow()],
   nitro: {
     serverDir: './',
-    // hooks: {
-    //   // Start the Postgres World
-    //   // Needed since we test this in CI
-    //   compiled: async () => {
-    //     if (process.env.WORKFLOW_TARGET_WORLD === '@workflow/world-postgres') {
-    //       const { getWorld } = await import('workflow/runtime');
-    //       console.log('Starting Postgres World...');
-    //       await getWorld().start?.();
-    //     }
-    //   },
-    // },
+    plugins: ['plugins/start-pg-world.ts'],
   },
 });
