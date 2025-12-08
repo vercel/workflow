05d14719f4643e111a834d8c99eddbe084f028aa
Peter Wielander <mittgfu@gmail.com>
2025-12-08 13:07:42 -0800

    List streams endpoint

--- packages/world/src/interfaces.ts
+++ packages/world/src/interfaces.ts
@@ -42,6 +42,7 @@ export interface Streamer {
     name: string,
     startIndex?: number
   ): Promise<ReadableStream<Uint8Array>>;
+  listStreams(runId: string): Promise<string[]>;
 }
 
 export interface Storage {
