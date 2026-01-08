9eac3726b045cf46f6114a5af5febfa09f966b85
Nathan Rajlich <n@n8.io>
2026-01-08 12:32:42 -0800

    .

--- packages/world-local/src/streamer.ts
+++ packages/world-local/src/streamer.ts
@@ -174,7 +174,7 @@ export function createStreamer(basedir: string): Streamer {
       return data?.streams ?? [];
     },
 
-    async readFromStream(name: string, startIndex = 0, _runId?: string) {
+    async readFromStream(name: string, startIndex = 0) {
       const chunksDir = path.join(basedir, 'streams', 'chunks');
       let removeListeners = () => {};
 
