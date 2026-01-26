fdf0013e28061cc654c7141253db9a83ac647e08
Nathan Rajlich <n@n8.io>
2026-01-26 14:57:10 -0800

    .

--- packages/world-vercel/src/utils.ts
+++ packages/world-vercel/src/utils.ts
@@ -14,8 +14,7 @@ import { version } from './version.js';
  *
  * Example: 'https://workflow-server-git-branch-name.vercel.sh'
  */
-const WORKFLOW_SERVER_URL_OVERRIDE =
-  'https://workflow-server-git-01-23-acceptcbor-encodedrequestbodies.vercel.sh';
+const WORKFLOW_SERVER_URL_OVERRIDE = '';
 
 export interface APIConfig {
   token?: string;
