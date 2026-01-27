164afaf6eb92d13272a8a36ec45ee1bb302417cb
Nathan Rajlich <n@n8.io>
2026-01-27 13:03:40 -0800

    Use production workflow-server

--- packages/world-vercel/src/utils.ts
+++ packages/world-vercel/src/utils.ts
@@ -14,8 +14,7 @@ import { version } from './version.js';
  *
  * Example: 'https://workflow-server-git-branch-name.vercel.sh'
  */
-const WORKFLOW_SERVER_URL_OVERRIDE =
-  'https://workflow-server-git-01-27-implementmultiwritemodeforstre-1e1eee.vercel.sh';
+const WORKFLOW_SERVER_URL_OVERRIDE = '';
 
 export interface APIConfig {
   token?: string;
