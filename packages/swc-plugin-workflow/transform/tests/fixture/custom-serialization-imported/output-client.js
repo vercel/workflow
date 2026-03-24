b1a5b702af525e8d4079a61e2ce5e4bf2e0c8368
Pranay Prakash <pranay.gp@gmail.com>
2026-03-23 17:40:31 -0700

    Revert "Inline class serialization registration to fix 3rd-party package supp…"
    This reverts commit 7dcddb5f33594b93e06dfe3cf5ad1207a988dfac.

--- packages/swc-plugin-workflow/transform/tests/fixture/custom-serialization-imported/output-client.js
+++ packages/swc-plugin-workflow/transform/tests/fixture/custom-serialization-imported/output-client.js
@@ -1,3 +1,4 @@
+import { registerSerializationClass } from "workflow/internal/class-serialization";
 // Test custom serialization with imported symbols from '@workflow/serde'
 import { WORKFLOW_SERIALIZE, WORKFLOW_DESERIALIZE } from '@workflow/serde';
 /**__internal_workflows{"classes":{"input.js":{"Color":{"classId":"class//./input//Color"},"Vector":{"classId":"class//./input//Vector"}}}}*/;
@@ -38,23 +39,5 @@ export class Color {
         return new Color(data.r, data.g, data.b);
     }
 }
-(function(__wf_cls, __wf_id) {
-    var __wf_sym = Symbol.for("workflow-class-registry"), __wf_reg = globalThis[__wf_sym] || (globalThis[__wf_sym] = new Map());
-    __wf_reg.set(__wf_id, __wf_cls);
-    Object.defineProperty(__wf_cls, "classId", {
-        value: __wf_id,
-        writable: false,
-        enumerable: false,
-        configurable: false
-    });
-})(Color, "class//./input//Color");
-(function(__wf_cls, __wf_id) {
-    var __wf_sym = Symbol.for("workflow-class-registry"), __wf_reg = globalThis[__wf_sym] || (globalThis[__wf_sym] = new Map());
-    __wf_reg.set(__wf_id, __wf_cls);
-    Object.defineProperty(__wf_cls, "classId", {
-        value: __wf_id,
-        writable: false,
-        enumerable: false,
-        configurable: false
-    });
-})(Vector, "class//./input//Vector");
+registerSerializationClass("class//./input//Color", Color);
+registerSerializationClass("class//./input//Vector", Vector);
