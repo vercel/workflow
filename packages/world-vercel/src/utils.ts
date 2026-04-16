552c2eb567194aa4ec096d3748839e8542c4869a
Peter Wielander <mittgfu@gmail.com>
2026-04-15 19:34:22 -0700

    Revert "Restore WORKFLOW_SERVER_URL_OVERRIDE for e2e testing"
    This reverts commit 80c756f546f9dded7f1e295e4e60ce678622abab.

--- packages/world-vercel/src/utils.ts
+++ packages/world-vercel/src/utils.ts
@@ -61,8 +61,7 @@ function httpLog(
  *
  * Example: 'https://workflow-server-git-branch-name.vercel.sh'
  */
-const WORKFLOW_SERVER_URL_OVERRIDE =
-  'https://workflow-server-git-peter-stream-control-frame.vercel.sh';
+const WORKFLOW_SERVER_URL_OVERRIDE = '';
 
 export interface APIConfig {
   token?: string;
